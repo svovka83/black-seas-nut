4da4b32e9a717b3c658dbac3d9e839fabb92e1b7
Volodymyr Smoliak <svovka83@gmail.com>
2026-05-14 18:37:30 +0300

    marker

--- src/js/api/init-map.js
+++ src/js/api/init-map.js
@@ -5,7 +5,7 @@ export function initMap() {
 		attribution: "&copy; OpenStreetMap &copy; CARTO",
 	}).addTo(map);
 
-	L.marker([52.053931, 1.134981]).addTo(map);
+	const marker = L.marker([52.053931, 1.134981]).addTo(map);
 
 	map.scrollWheelZoom.disable();
 
@@ -15,6 +15,12 @@ export function initMap() {
 
 	map.on("mouseout", () => {
 		map.scrollWheelZoom.disable();
+	});
+
+	marker.on("click", () => {
+		map.flyTo([52.053931, 1.134981], 16, {
+			duration: 1.5,
+		});
 	});
 
 	const mapElement = document.getElementById("map");
