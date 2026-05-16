9647faa4d4281d92de748dddcf28bc615aedce29
Volodymyr Smoliak <svovka83@gmail.com>
2026-05-16 19:15:30 +0300

    product-card

--- src/js/ui/button.js
+++ src/js/ui/button.js
@@ -1,8 +1,11 @@
 export function getButton() {
 	const headerButton = document.querySelector(".header-middle__button");
+	const productButton = document.querySelector(".product-card__bottom-button");
 	if (!headerButton) return;
+	if (!productButton) return;
 
 	headerButton.innerHTML = button(false, "Заказать звонок");
+	productButton.innerHTML = button(false, "Купить");
 }
 
 function button(isActive = false, titleNormal = "Заказать звонок", titleActive = "") {
